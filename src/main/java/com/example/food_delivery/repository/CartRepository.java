a1e916df30b832beaed498828130ac85c5e42836
Muhammadali <akbarovmuhammadali777@gmail.com>
2024-11-27 20:14:02 +0500

    order status change added

--- src/main/java/com/example/food_delivery/repository/CartRepository.java
+++ src/main/java/com/example/food_delivery/repository/CartRepository.java
@@ -2,6 +2,7 @@ package com.example.food_delivery.repository;
 
 import com.example.food_delivery.entity.AuthUser;
 import com.example.food_delivery.entity.Cart;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
